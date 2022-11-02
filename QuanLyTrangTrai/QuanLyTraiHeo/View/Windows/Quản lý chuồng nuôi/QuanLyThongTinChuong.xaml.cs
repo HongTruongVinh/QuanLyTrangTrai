03f0c948c478f0d30df04df3b6c6663be34be459
HongTruongVinh <20522157@gm.uit.edu.vn>
2022-11-02 10:03:05 +0700

    Tao giao dien QuanLyChuongNuoi, QuanLyNhanVien, QuanLyNhatKy, ThietLapCayMucTieu

--- QuanLyTrangTrai/QuanLyTraiHeo/View/Windows/Quản lý chuồng nuôi/QuanLyThongTinChuong.xaml.cs
+++ QuanLyTrangTrai/QuanLyTraiHeo/View/Windows/Quản lý chuồng nuôi/QuanLyThongTinChuong.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuanLyTraiHeo.View.Windows.Quản_lý_chuồng_nuôi;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
--- QuanLyTrangTrai/QuanLyTraiHeo/View/Windows/Quản lý nhân viên/QuanLyThongTinNhanVienWindow.xaml.cs
+++ QuanLyTrangTrai/QuanLyTraiHeo/View/Windows/Quản lý nhân viên/QuanLyThongTinNhanVienWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuanLyTraiHeo.View.Windows.Quản_lý_nhân_viên;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
